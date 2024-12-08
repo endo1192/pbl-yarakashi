b6ccdd4ec48b79078c464c2d5996fa9b85ace19e
endo1192 <mirin2316@gmail.com>
2024-12-08 21:18:16 +0900

    exper

--- src/pages/Kmenu.tsx
+++ src/pages/Kmenu.tsx
@@ -77,7 +77,7 @@ export default function Home() {
       <Link href="/" className=" px-4 py-4 gap-4">ホームに戻る</Link>
       <Link href="/bbs-posts/create" className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md">投稿</Link>
       <br /><br /> 
-      <h1>やらかし掲示板</h1>
+      <h1>やらかし掲示板k</h1>
       <br />
       <main>
         <CardList bCardList={bCardList} /> {/* bCardListをCardListに渡す */}
